a15eeb92c573f0c8605e467cc392ea0352fde0ae
SoraAsc <soragg59@gmail.com>
2020-03-27 14:20:50 -0300

    Teste e Validações

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,7 +31,8 @@
 
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes')
+const { errors } = require('celebrate');
+const routes = require('./routes');
 
 
 const app = express();
@@ -39,5 +40,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
+//app.listen(3333);
